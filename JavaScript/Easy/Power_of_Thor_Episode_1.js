139f5d7e1146e967578992411c6d7a076d70a68c
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-08-09 14:45:18 +0300

    Add solution for 'Power of Thor - Episode 1'

---
+++ JavaScript/Easy/Power_of_Thor_Episode_1.js
@@ -0,0 +1,33 @@
+var inputs = readline().split(' ');
+const lightX = parseInt(inputs[0]); // the X position of the light of power
+const lightY = parseInt(inputs[1]); // the Y position of the light of power
+const initialTx = parseInt(inputs[2]); // Thor's starting X position
+const initialTy = parseInt(inputs[3]); // Thor's starting Y position
+
+let diffX = lightX - initialTx;
+let diffY = lightY - initialTy;
+
+// game loop
+while (true) {
+    const remainingTurns = parseInt(readline()); // The remaining amount of turns Thor can move. Do not remove this line.
+
+    let direction = '';
+
+    if (diffY > 0) {
+        direction += 'S';
+        diffY--;
+    } else if (diffY < 0) {
+        direction += 'N';
+        diffY++;
+    }
+
+    if (diffX > 0) {
+        direction += 'E';
+        diffX--;
+    } else if (diffX < 0) {
+        direction += 'W';
+        diffX++;
+    }
+
+    console.log(direction);
+}
