34b34de4931f8ab012c02dc3024323c4a7651b02
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-08-09 15:02:07 +0300

    Add solution for 'The Descent'

---
+++ JavaScript/Easy/The_Descent.js
@@ -0,0 +1,10 @@
+// game loop
+while (true) {
+  const heights = [...Array(8)].map(_ => parseInt(readline()));
+
+  const highestIdx = heights.reduce((highestIdx, currHeight, idx, heights) => {
+      return (currHeight > heights[highestIdx]) ? idx : highestIdx;
+  }, 0);
+
+  console.log(highestIdx);
+}
